2bf41d990b2d975db430d217fe876f50985ac4d7
laurent420 <laurent.pladet@student.ehb.be>
2023-08-17 21:04:57 +0200

    test

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tutoring Platform</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <!-- Registratie en Inloggen -->
+    <div id="registration-login">
+        <form id="registration-form">
+            <!-- Registratieformulier -->
+        </form>
+        <form id="login-form">
+            <!-- Inlogformulier -->
+        </form>
+    </div>
+
+    <!-- Vakken Toevoegen -->
+    <div id="add-subject">
+        <form id="add-subject-form">
+            <!-- Formulier voor vakken toevoegen -->
+        </form>
+    </div>
+
+    <!-- Tutoring Aanvragen -->
+    <div id="request-tutoring">
+        <form id="request-tutoring-form">
+            <!-- Formulier voor tutoring aanvragen -->
+        </form>
+    </div>
+
+    <!-- Openstaande Aanvragen Bekijken -->
+    <div id="view-open-requests">
+        <!-- Lijst van openstaande aanvragen -->
+    </div>
+
+    <!-- Gamification en Feedback -->
+    <div id="gamification-feedback">
+        <div id="satisfaction-section">
+            <!-- "Bent u tevreden over deze Tutor" gedeelte -->
+        </div>
+        <div id="score-system">
+            <!-- Scoresysteem met gamification -->
+        </div>
+        <div id="question-feedback">
+            <!-- Mogelijkheid om vragen te stellen, antwoorden te geven en feedback te geven -->
+        </div>
+    </div>
+
+    <script src="scripts.js"></script>
+</body>
+</html>
